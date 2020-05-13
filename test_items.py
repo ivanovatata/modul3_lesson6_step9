5f7b52f199651ec40c62617280be1ef0f6275bdd
mail@ma.com <testmail@email.com>
2020-05-13 21:59:44 +0300

    Initial commit of files for modul 3 lesson 6 step 9

---
+++ test_items.py
@@ -0,0 +1,13 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+def test_cart_button(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    browser.implicitly_wait(30)
+
+    button = browser.find_element_by_css_selector("button.btn-add-to-basket")
+    assert button, "There is not element 'Add to cart' on the page"
+
